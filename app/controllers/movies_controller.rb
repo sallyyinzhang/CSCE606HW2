980069301f18af8e5291a2ea432f65c4df9752bd
EC2 Default User <ec2-user@ip-172-31-19-131.us-east-2.compute.internal>
2019-02-11 20:20:42 +0000

    roolback

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    # @movies = Movie.all
+    @movies = Movie.all
     # @movies = Movie.all.order(params[:sort_by] + " ASC")
     # @sort_column = params[:sort_by]
   end
