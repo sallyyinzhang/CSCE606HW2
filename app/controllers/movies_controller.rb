80b4f3c5bcecccba70076f809485a3fd31eda28d
EC2 Default User <ec2-user@ip-172-31-19-131.us-east-2.compute.internal>
2019-02-11 20:14:23 +0000

    change css

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,9 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    # @movies = Movie.all
+    @movies = Movie.all.order(params[:sort_by] + " ASC")
+    @sort_column = params[:sort_by]
   end
 
   def new
