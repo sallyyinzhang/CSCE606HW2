34e839ef75a4d090668a7b837c7736362f1ce034
EC2 Default User <ec2-user@ip-172-31-19-131.us-east-2.compute.internal>
2019-02-11 20:58:22 +0000

    test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,6 +3,10 @@ class MoviesController < ApplicationController
 #  def movie_params
 #    params.require(:movie).permit(:title, :rating, :description, :release_date)
 #  end
+  def initialize
+        @all_ratings = Movie.all_ratings
+        super
+  end
 
   def show
     id = params[:id] # retrieve movie ID from URI route
